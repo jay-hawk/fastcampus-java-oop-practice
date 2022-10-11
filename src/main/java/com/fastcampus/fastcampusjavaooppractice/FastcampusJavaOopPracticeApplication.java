faef01cd57043b74f92a3cc74a56dc9c5bcd782c
RyuJaeHyuk <jayhawk.dev@gmail.com>
2022-10-11 23:19:55 +0900

    Initialize java + gradle
    FastCampus 예제 JAVA + gradle Project Setup

---
+++ src/main/java/com/fastcampus/fastcampusjavaooppractice/FastcampusJavaOopPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusjavaooppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusJavaOopPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusJavaOopPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcampus/fastcampusjavaooppractice/FastcampusJavaOopPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusjavaooppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusJavaOopPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
